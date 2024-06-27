0a52e28f2710ab894cea5d3ecb5731a8f69737c7
thanh <thanhncph30708@fpt.edu.vn>
2024-06-27 16:09:37 +0700

    update

--- app/src/main/java/thanhncph30708/fpoly/lab1_nc/Item.java
+++ app/src/main/java/thanhncph30708/fpoly/lab1_nc/Item.java
@@ -1,2 +1,37 @@
-package thanhncph30708.fpoly.lab1_nc;public class Item {
+package thanhncph30708.fpoly.lab1_nc;
+
+public class Item {
+    private long id;
+    private String itemName;
+    private int age;
+
+    public Item(long id, String itemName, int age) {
+        this.id = id;
+        this.itemName = itemName;
+        this.age = age;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
